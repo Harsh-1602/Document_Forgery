26e84235ed986c5c91e81f438be222bec110de38
Harsh <harshg.bt.20@nitj.ac.in>
2023-10-20 18:28:23 +0530

    New commit

--- Model_deploy/app.py
+++ Model_deploy/app.py
@@ -1,13 +1,21 @@
 from flask import Flask,request,render_template
+import pickle
 import os 
 from werkzeug.utils import secure_filename
+import ultralytics
+from ultralytics import YOLO
+from PIL import Image
+
 app=Flask(__name__)
 
 
 
-# model = load_model('model/model.h5')
+# model = pickle.load(open('Model_deploy/models/Whitener.pkl','rb'))
+model= YOLO("Model_deploy/models/best.pt")
 
-upload_folder=os.path.join('static','uploads')
+upload_folder=os.path.join('Model_deploy','static','uploads')
+predicted_folder=os.path.join('Model_deploy','static','Predicted')
+app.config['PREDICTED']=predicted_folder
 app.config['UPLOAD']=upload_folder
 @app.route('/')
 def home():
@@ -32,6 +40,18 @@ def predict():
     # #     else:
     #         output="NOT Sarcasm"
     # return render_template("index.html",prediction_text="{}".format(output))
+    # n_img=img.resize((640,640))
+    # img=n_img
+
+    imag=model.predict(img)
+    img_arr=imag[0].plot()
+    pred=Image.fromarray(img_arr[...,::-1])
+
+    pred.save(os.path.join(app.config['PREDICTED'], filename))
+
+    # print(type(pred))
+    img1 = os.path.join(app.config['PREDICTED'], filename)
+
     return render_template("final.html",img=img)
 
 
