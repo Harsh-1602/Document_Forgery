c5f4828b653958f7feb2f26611367800dae0e87e
Mohit <mkbhagat50.mb@gmail.com>
2023-10-22 17:32:06 +0530

    .

--- application.py
+++ application.py
@@ -8,7 +8,7 @@ from PIL import Image
 import cv2
 import numpy as np
 
-app=Flask(__name__)
+application=Flask(__name__)
 
 
 
@@ -167,4 +167,4 @@ def predict():
 
 
 if(__name__=="__main__"):
-    app.run(host="0.0.0.0", port=8080)
+    app.run(host="0.0.0.0")
